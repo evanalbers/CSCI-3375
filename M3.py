0ae4d7915ea9a709b203b6a34273094daf48a83c
Evan Albers <ealbers@bowdoin.edu>
2022-09-14 20:28:51 -0400

    tuning MC implemented

--- M3.py
+++ M3.py
@@ -8,33 +8,75 @@ NOTES: Want to think about having turtles that start out "excited" - fast moving
 
 
 
+
 import numpy as np
 import turtle as t
+import sys
+
+"""
+markov tuner is a function that lets the user "tune" the markov table to achieve a given desired outcome
+"""
+def markov_tuner(state, additional_chance, matrix):
+
+    for key in list(matrix.keys()):
+
+        current_prob = matrix[key][state]
+        if current_prob + additional_chance > 1:
+
+            for element in matrix[key]:
+                if element == state:
+                    matrix[key][state] = 1
+                else:
+                    matrix[key][element] = 0
+        else: 
+            matrix[key][state] = matrix[key][state] + additional_chance
+            for element in matrix[key]:
+                if element != state:
+                    matrix[key][element] = (1 - matrix[key][state]) / (matrix[key].size() - 1)
+            
+
+    
 
 
-def roadLessTurtled():
+def roadLessTurtled(tuning_coeff):
 
-    transition_matrix = {   0 : {0 : 0.1, 1 : 0.9, 2 : 0, 3 : 0, 4 : 0, 5 : 0, 6 : 0, 7 : 0, 8 : 0, 9 : 0},
-                            1 : {0 : 0.2, 1 : 0, 2 : 0.8, 3 : 0, 4 : 0, 5 : 0, 6 : 0, 7 : 0, 8 : 0, 9 : 0},
-                            2 : {0 : 0.3, 1 : 0, 2 : 0, 3 : 0.7, 4 : 0, 5 : 0, 6 : 0, 7 : 0, 8 : 0, 9 : 0},
-                            3 : {0 : 0.4, 1 : 0, 2 : 0, 3 : 0, 4 : 0.6, 5 : 0, 6 : 0, 7 : 0, 8 : 0, 9 : 0},
-                            4 : {0 : 0.5, 1 : 0, 2 : 0, 3 : 0, 4 : 0, 5 : 0.5, 6 : 0, 7 : 0, 8 : 0, 9 : 0},
-                            5 : {0 : 0.6, 1 : 0, 2 : 0, 3 : 0, 4 : 0, 5 : 0, 6 : 0.4, 7 : 0, 8 : 0, 9 : 0},
-                            6 : {0 : 0.7, 1 : 0.9, 2 : 0, 3 : 0, 4 : 0, 5 : 0, 6 : 0, 7 : 0.3, 8 : 0, 9 : 0},
-                            7 : {0 : 0.8, 1 : 0.9, 2 : 0, 3 : 0, 4 : 0, 5 : 0, 6 : 0, 7 : 0, 8 : 0.2, 9 : 0},
-                            8 : {0 : 0.9, 1 : 0.9, 2 : 0, 3 : 0, 4 : 0, 5 : 0, 6 : 0, 7 : 0, 8 : 0, 9 : 0.1},
-                            9 : {0 : 0.1, 1 : 0.9, 2 : 0, 3 : 0, 4 : 0, 5 : 0, 6 : 0, 7 : 0, 8 : 0, 9 : 0.9},
+
+    if type(tuning_coeff) != int or tuning_coeff < 1:
+        tuning_coeff = 1
+
+    prob = 0.1 / int(tuning_coeff)
+
+    print(prob)
+
+    transition_matrix = {   0 : {0 : prob, 1 : 1-prob, 2 : 0, 3 : 0, 4 : 0, 5 : 0, 6 : 0, 7 : 0, 8 : 0, 9 : 0},
+                            1 : {0 : 2 * prob, 1 : 0, 2 : 1-( 2* prob), 3 : 0, 4 : 0, 5 : 0, 6 : 0, 7 : 0, 8 : 0, 9 : 0},
+                            2 : {0 : 3 * prob, 1 : 0, 2 : 0, 3 :  1-(3 * prob), 4 : 0, 5 : 0, 6 : 0, 7 : 0, 8 : 0, 9 : 0},
+                            3 : {0 : 4 * prob, 1 : 0, 2 : 0, 3 : 0, 4 : 1-(4 * prob), 5 : 0, 6 : 0, 7 : 0, 8 : 0, 9 : 0},
+                            4 : {0 : 5 * prob, 1 : 0, 2 : 0, 3 : 0, 4 : 0, 5 : 1-(5 * prob), 6 : 0, 7 : 0, 8 : 0, 9 : 0},
+                            5 : {0 : 6 * prob, 1 : 0, 2 : 0, 3 : 0, 4 : 0, 5 : 0, 6 : 1-(6 * prob), 7 : 0, 8 : 0, 9 : 0},
+                            6 : {0 : 7 * prob, 1 : 0, 2 : 0, 3 : 0, 4 : 0, 5 : 0, 6 : 0, 7 : 1-(7 * prob), 8 : 0, 9 : 0},
+                            7 : {0 : 8 * prob, 1 : 0, 2 : 0, 3 : 0, 4 : 0, 5 : 0, 6 : 0, 7 : 0, 8 : 1-(8 * prob), 9 : 0},
+                            8 : {0 : 9 * prob, 1 : 0, 2 : 0, 3 : 0, 4 : 0, 5 : 0, 6 : 0, 7 : 0, 8 : 0, 9 :1-(9 * prob)},
+                            9 : {0 : prob, 1 : 0, 2 : 0, 3 : 0, 4 : 0, 5 : 0, 6 : 0, 7 : 0, 8 : 0, 9 : 1 - prob},
     }
+    text_turt = t.Turtle()
+    text_turt.penup()
+    text_turt.goto(-50, 200)
+    text_turt.write("Tuning Coefficient: " + str(tuning_coeff), move=False, align="left", font="Verdana")
+    text_turt.hideturtle()
 
-    for m in range(100):
+    for m in range(25):
         turt = t.Turtle()
         turt.penup()
         turt.goto(0, -50)
         turt.pendown()
         state = 0
         turt.speed(0)
-        for n in range(m):
-            new_state = np.random.choice(list(transition_matrix[state].keys()), 1, list(transition_matrix[state].values()))[0]
+        
+        for n in range(24):
+            new_state = np.random.choice(list(transition_matrix[state].keys()), 1, True, list(transition_matrix[state].values()))[0]
+            print(list(transition_matrix[state].keys()))
+            print(list(transition_matrix[state].values()))
             print(new_state)
             if new_state > 0:
                 turt.color("black")
@@ -50,9 +92,11 @@ def roadLessTurtled():
 
 def main():
 
-    roadLessTurtled()
+    
+
+    roadLessTurtled(sys.argv[1])
     t.done()
-    t.mainloop()
+    #t.mainloop()
     
     
     
